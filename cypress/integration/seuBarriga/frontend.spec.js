d13626fafa2e15f96da86d80d76429ca8e418f2b
Siqueira Neto <siqueiranetoufc@gmail.com>
2024-01-10 14:40:57 -0300

    criacao de teste usando propria masssa de dados finalizando os testes

--- cypress/integration/seuBarriga/frontend.spec.js
+++ cypress/integration/seuBarriga/frontend.spec.js
@@ -2,47 +2,19 @@
 
 import loc from '../../support/locators'
 import '../../support/commandsContas'
+import buildEnv from '../../support/buildEnv'
 
 describe('Should test at a funcional level', () =>{
 
     after(()=>{
         cy.clearLocalStorage()
     })
-    
-    before(() =>{
-        cy.server()
-        cy.route({
-            method: 'POST',
-            url:'/signin',
-            response:{
-                id: 1000,
-                nome: 'Usuario falso',
-                token: 'Uma string muito grande que nao ser deveria ser aceita'
-            }
-        }).as('signin')
-        cy.route({
-            method:'GET',
-            url: '/saldo',
-            response: [{
-                conta_id: 999,
-                conta: "Conta falsa 1",
-                saldo: "100.00"
-            },
-            {
-                conta_id: 9909,
-                conta: "Conta falsa 2",
-                saldo: "10000000000000.00"
-            }
-            ]
-        }).as('saldo')
-        cy.login('neto@neto.com', '12345')    
-       // cy.resetApp()
-        
-    })
 
     beforeEach(() =>{
+        buildEnv()
+        cy.login('neto@neto.com', '12345') 
         cy.get(loc.MENU.HOME).click()
-        
+        // cy.resetApp()
     })
 
 
@@ -75,7 +47,7 @@ describe('Should test at a funcional level', () =>{
                 visivel:true,
                 usuario_id:1
             }
-        })
+        }).as('saveContas')
 
        cy.acessarMenuConta()
 
@@ -108,26 +80,8 @@ describe('Should test at a funcional level', () =>{
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     });
 
-    it.only('Should update an account', () => {
-        cy.route({
-            method: 'GET',
-            url:'/contas',
-            response:[
-             {
-                 id: 1,
-                 nome: "Carteira",
-                 visivel: true,
-                 usuario_id: 1
-             },
-             {
-                 id: 2,
-                 nome: "bancos",
-                 visivel: true,
-                 usuario_id: 1
-             }
-             ] 
-         }).as('contas')
-
+    it('Should update an account', () => {
+      
          cy.route({
             method:'PUT',
             url:'/contas/**',
@@ -170,6 +124,15 @@ describe('Should test at a funcional level', () =>{
     });
 
     it('Should not create an account with same name', () => {
+        cy.route({
+            method:'POST',
+            url:'/contas',
+            response:{
+                error: "Ja existe uma conta com esse nome!"
+            },
+            status: 400
+        }).as('saveContasMesmoNome')
+
         cy.acessarMenuConta()
 
         cy.get(loc.CONTA.NOME).type('Conta mesmo nome')
@@ -178,20 +141,88 @@ describe('Should test at a funcional level', () =>{
     });
 
     it('Should create a transaction', () => {
+        cy.route({
+            method:'POST',
+            url:'/transacoes',
+            response:{
+                id: 1885514,
+                descricao: "dqqwewq",
+                envolvido: "dasdasdas",
+                observacao: null,
+                tipo: "REC",
+                data_transacao: "2024-01-10T03:00:00.000Z",
+                data_pagamento: "2024-01-10T03:00:00.000Z",
+                valor: "232323.00",
+                status: false,
+                conta_id: 2010514,
+                usuario_id: 35269,
+                transferencia_id: null,
+                parcelamento_id: null
+            }
+        })
+
+        cy.route({
+            method: 'GET',
+            url:'/extrato/**',
+            response:'fixture:movimentacaoSalva'
+         })
+
         cy.get(loc.MENU.MOVIMENTACAO).click()
         cy.get(loc.MOVIMENTO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTO.VALOR).type('123')
         cy.get(loc.MOVIMENTO.INTERESSADO).type('Int')
-        cy.get(loc.MOVIMENTO.CONTA_MOV).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTO.CONTA_MOV).select('bancos')
         cy.get(loc.MOVIMENTO.BNT_STATUS).click()
         cy.get(loc.MOVIMENTO.BNT_MOV).click()
+
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
     });
 
 
     it('Should get Balance', () => {
+        cy.route({
+            method: 'GET',
+            url:'/transacoes/**',
+            response:{
+                "conta": "Conta para saldo",
+                "id": 1885517,
+                "descricao": "Movimentacao 1, calculo saldo",
+                "envolvido": "CCC",
+                "observacao": null,
+                "tipo": "REC",
+                "data_transacao": "2024-01-10T03:00:00.000Z",
+                "data_pagamento": "2024-01-10T03:00:00.000Z",
+                "valor": "3500.00",
+                "status": false,
+                "conta_id": 2011571,
+                "usuario_id": 35269,
+                "transferencia_id": null,
+                "parcelamento_id": null
+            }
+        })
+        cy.route({
+            method: 'PUT',
+            url:'/transacoes/**',
+            response:{
+                "conta": "Conta para saldo",
+                "id": 1885517,
+                "descricao": "Movimentacao 1, calculo saldo",
+                "envolvido": "CCC",
+                "observacao": null,
+                "tipo": "REC",
+                "data_transacao": "2024-01-10T03:00:00.000Z",
+                "data_pagamento": "2024-01-10T03:00:00.000Z",
+                "valor": "3500.00",
+                "status": false,
+                "conta_id": 2011571,
+                "usuario_id": 35269,
+                "transferencia_id": null,
+                "parcelamento_id": null
+            }
+        })
+
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '100,00')
 
         cy.get(loc.MENU.EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
@@ -205,12 +236,34 @@ describe('Should test at a funcional level', () =>{
         cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
         cy.get(loc.MOVIMENTO.BNT_MOV).click()
 
+        cy.route({
+            method:'GET',
+            url: '/saldo',
+            response: [{
+                conta_id: 999,
+                conta: "Carteira",
+                saldo: "4034.00"
+            },
+            {
+                conta_id: 9909,
+                conta: "Conta falsa 2",
+                saldo: "10000000000000.00"
+            }
+            ]
+        }).as('saldoFinal')
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Carteira')).should('contain', '4.034,00')
 
     });
 
     it('Should remove a transaction', () => {
+        cy.route({
+            method: 'DELETE',
+            url:'/transacoes/**',
+            response:{},
+            status: 204
+        }).as('del')
+
         cy.get(loc.MENU.EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
